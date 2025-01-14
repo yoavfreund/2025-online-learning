b0ffdf6cf2d249ae507add98fdd43e9a8c08ca51
Yoav Freund <yfreund@ucsd.edu>
2025-01-13 17:31:55 -0800

    evening before

---
+++ 6.mixable/auto/talk7.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "talk7"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "english") ("inputenc" "latin1") ("fontenc" "T1")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "../macros"
+    "beamer"
+    "beamer10"
+    "xmpmulti"
+    "babel"
+    "inputenc"
+    "times"
+    "fontenc"))
+ :latex)
+
