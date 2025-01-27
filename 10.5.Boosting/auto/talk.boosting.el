5026faff42dc292c6649cdb903eb66f502d637ce
Yoav Freund <yfreund@ucsd.edu>
2025-01-27 14:48:27 -0800

    presentataion

--- 10.5.Boosting/auto/talk.boosting.el
+++ 10.5.Boosting/auto/talk.boosting.el
@@ -1,17 +1,15 @@
 (TeX-add-style-hook
  "talk.boosting"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("beamer" "handout")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "english") ("inputenc" "utf8") ("fontenc" "T1")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
